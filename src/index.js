49e21a71ac320ac08315f0bda2fcf628d24ecc36
mcshakes <efmccarthy26@gmail.com>
2020-12-11 15:39:23 -0700

    User can choose the size of board to start

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+function renderBoard() {
+    
+    let input_height = document.getElementById('height').value;
+    let input_width = document.getElementById('width').value;
+    
+    let parentContainer =  document.getElementsByClassName("game-container")[0];
+
+    let canvas = document.createElement("canvas");
+
+    canvas.id = "game-board";
+    canvas.width = input_width;
+    canvas.height = input_height;
+    canvas.style.zIndex   = 8;
+    canvas.style.position = "absolute";
+    canvas.style.border   = "1px solid black";
+
+    parentContainer.appendChild(canvas);
+    
+    hideForm()
+}
+
+function hideForm() {
+    // hiding the entire section
+    let form = document.getElementsByClassName("boardInputs")[0];
+    form.style.display = "none";
+}
