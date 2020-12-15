70a8df2abeb5774d2297e23618d56561e47d39c9
mcshakes <efmccarthy26@gmail.com>
2020-12-15 16:02:49 -0700

    Fixed, and now back to pre-render

--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
-var canvas;
-var context;
-const resolution = 40;
+
 
 function hideForm() {
-    // hiding the entire section
     let form = document.getElementsByClassName("boardInputs")[0];
     form.style.display = "none";
 }
 
-function startGameFlow() {
+function getUserInput() {
     let height = document.getElementById('height').value;
     let width = document.getElementById('width').value;
 
-    [COLS, ROWS] = buildBoard(height, width)
-    hideForm()
-    grid = buildGrid()
-    
-    nextGen(grid, COLS, ROWS);
-    render(grid);
-    requestAnimationFrame(update);
-}
-
-function buildGrid() {
-    const COLS = canvas.width / resolution;
-    const ROWS = canvas.height / resolution;
-
-    return new Array(COLS).fill(null)
-        .map(() => new Array(ROWS).fill(0)
-            .map(() => Math.floor(Math.random() * 2)));  // random fill of 1 or 0
+    return [height, width]    
 }
 
 function buildBoard(height, width) {    
@@ -43,81 +25,83 @@ function buildBoard(height, width) {
     canvas.style.position = "absolute";
     canvas.style.border   = "1px solid black";
 
-    const COLS = canvas.width / resolution;
-    const ROWS = canvas.height / resolution;
+    COLS = canvas.width / resolution;
+    ROWS = canvas.height / resolution;
 
     parentContainer.appendChild(canvas);
-    return [COLS, ROWS]
 }
 
+function create2DArray(cols, rows) {
+    let arr = new Array(cols);
 
-function render(grid) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+function fillRandom(cols, rows) {
+    for (let i = 0; i < cols - 1; i++) {
+        for (let j = 0; j < rows - 1; j++) {
+            let num = (Math.random() * 2);
+            let randomBinary = Math.floor(num);
+            if (randomBinary === 1) {
+                theGrid[i][j] = 1;
+            } else {
+                theGrid[i][j] = 0;
+            }
+        }
+    }
+}
+
+function drawGrid(grid) {
+    
     for (let col = 0; col < grid.length; col++) {
         for (let row = 0; row < grid[col].length; row++) {
-            const cell = grid[col][row]
+            const cell = grid[col][row];
 
             context.beginPath();
-            context.rect(col * resolution, row * resolution, resolution, resolution);
-            context.fillStyle = cell ? "black" : "white";
+            context.rect(col * resolution, row * resolution, resolution, resolution)
+            context.fillStyle = cell ? "black" : "white"; //if true, color in
             context.fill()
-            context.stroke();
+            context.stroke()
         }
     }
 }
 
-
-
-function nextGen(grid, columns, rows) {
-    console.log("IN nextGen", grid)
-    const nextGen = grid.map(arr => [...arr]); // a copy of the grid array
-
+function updateGrid() {
     for (let col = 0; col < grid.length; col++) {
         for (let row = 0; row < grid[col].length; row++) {
-            const cell = grid[col][row]
-            let numNeighbors = 0;
+            let neighborCells = 0;
+            const cell = grid[col][row];
 
-            for (let i = -1; i < 2; i++) {
-                for (let j = -1; j < 2; j++) {
-                    // Create a 3 by 3 grid to check surrounding of the current cell (0,0)
 
-                    if (i === 0 && j === 0) {
-                        continue;
-                    }
 
-                    const x_cell = col + i;
-                    const y_cell = row + j;
 
-                    if (x_cell >= 0 && y_cell >= 0 && x_cell < columns && y_cell < rows) { // If you aren't in the edges
-                        const currentNeighbor = grid[col + i][row + j];
-                        numNeighbors += currentNeighbor;
-                    }
-
-                    
-                }
-            }
-
-            // apply rules here after we get all the neighbors to study
-
-            if (cell === 1 && numNeighbors < 2) {
-                nextGen[col][row] = 0;
-            }
-            else if (cell === 1 && numNeighbors > 3) {
-                nextGen[col][row] = 0;
-            } 
-            else if (cell === 0 && numNeighbors === 3) {
-                nextGen[col][row] = 1;
-            }
         }
     }
+}
 
-    return nextGen;
+var canvas;
+var context;
+const resolution = 20;
+var COLS;
+var ROWS;
+var theGrid;
+var mirrorGrid;
+
+function startGameFlow() {
+    [height, width] = getUserInput()
+    hideForm()
+    buildBoard(400, 400)
+
+    theGrid = create2DArray(COLS, ROWS)
+    mirrorGrid = create2DArray(COLS, ROWS);
+
+    fillRandom(COLS, ROWS);
+    drawGrid(theGrid)
 }
 
 
-function update(in_grid, columns, rows) {
-    console.log("IN UPDATE", grid)
-    grid = nextGen(in_grid, columns, rows);
-    render(grid);
-    requestAnimationFrame(update);
-}
+
 
